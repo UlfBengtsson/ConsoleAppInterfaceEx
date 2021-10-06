0aa13479fca8d902a82ac9cff7f71315e8d6c496
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-10-06 09:49:33 +0200

    break time

--- ConsoleAppInterfaceEx/Services/PeopleService.cs
+++ ConsoleAppInterfaceEx/Services/PeopleService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ConsoleAppInterfaceEx.Data;
+using ConsoleAppInterfaceEx.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,24 @@ namespace ConsoleAppInterfaceEx.Services
 {
     public class PeopleService
     {
+        IPeopleRepo _peopleRepo;
+
+        public PeopleService(IPeopleRepo peopleRepo)
+        {
+            _peopleRepo = peopleRepo;
+        }
+
+        Person Create(string firstName, string lastname)
+        {
+            //Person person = new Person();
+            //person.FirstName = firstName;
+            //person.LastName = lastname;
+
+            Person person = new Person() { FirstName = firstName, LastName = lastname };
+
+            person = _peopleRepo.Create(person);
+
+            return person;
+        }
     }
 }
