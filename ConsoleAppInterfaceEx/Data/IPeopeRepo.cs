127b52d6181e7d66207bd5d46127f7107fe09dc6
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-10-06 09:03:46 +0200

    Add project files.

---
+++ ConsoleAppInterfaceEx/Data/IPeopeRepo.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleAppInterfaceEx.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppInterfaceEx.Data
+{
+    public interface IPeopeRepo
+    {
+        //C
+        Person Create(Person person);
+
+        //R
+        Person FindById(Guid id);
+        List<Person> GetAll();
+
+        //U
+        void Update(Person person);
+
+        //D
+        void Delete(Person person);
+    }
+}
---
+++ ConsoleAppInterfaceEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppInterfaceEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
